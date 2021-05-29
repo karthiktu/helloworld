c490d7d0b3d271ce9cfa2efb6dad8610b62c45a0
karthiktu <tukarthik@gmail.com>
2021-05-29 12:20:47 +0530

    first commit

---
+++ src/main/java/com/tuk/Starter.java
@@ -0,0 +1,20 @@
+package com.tuk;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Starter {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World - v3!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Starter.class, args);
+    }
+
+}
